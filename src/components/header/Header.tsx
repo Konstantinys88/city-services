d80a5320490163e343e27da8475d03329fc381ad
Maschkov Konstantin <maschkovc@yandex.ru>
2023-12-05 14:03:45 +0300

    menu

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -8,29 +8,60 @@ import Link from 'next/link';
 const Header = () => {
 
 	const [dropdownRestaurants, setDropdownRestaurants] = useState(false);
+	const [dropdownEntertainments, setDropdownEntertainments] = useState(false);
+	const [dropdownShopping, setDropdownShopping] = useState(false);
 
 	const handleDropdownRestaurants = () => {
-		setDropdownRestaurants(!dropdownRestaurants)
-
+		setDropdownRestaurants(!dropdownRestaurants);
 	}
-
 	let dropdownStyleRestaurants = "header__dropdown-container";
 	dropdownRestaurants ? dropdownStyleRestaurants = "header__dropdown-container-active" : dropdownStyleRestaurants = "header__dropdown-container";
+
+	const handleDropdownEntertaiment = () => {
+		setDropdownEntertainments(!dropdownEntertainments);
+	}
+	let dropdownStyleEntertaiment = "header__dropdown-container";
+	dropdownEntertainments ? dropdownStyleEntertaiment = "header__dropdown-container-active" : dropdownStyleEntertaiment = "header__dropdown-container"
+
+	const handleDropdownShopping = () => {
+		setDropdownShopping(!dropdownShopping);
+	}
+	let dropdownStyleShopping = "header__dropdown-container";
+	dropdownShopping ? dropdownStyleShopping = "header__dropdown-container-active" : dropdownStyleShopping = "header__dropdown-container"
+
+
+
+
 
 	return (
 		<div className='header'>
 			<h1>CitiServices</h1>
 			<div className='header__nav'>
-				<Link href={`/`}>Главная</Link>
-				<Link href={`/about`}>О нас</Link>
-				<button className='button' onClick={handleDropdownRestaurants}>Еда<span> &#x25BC;</span></button>
+				<Link className='header__link' href={`/`}>Главная</Link>
+				<Link className='header__link' href={`/about`}>О нас</Link>
+				<button className={`${'button'} ${'header__link'}`} onClick={handleDropdownRestaurants}>Еда<span> &#x25BC;</span></button>
 				<div className={`${dropdownStyleRestaurants} ${'header__dropdown'}`}>
-					<Link href="#">Рестораны</Link>
-					<Link href="#">Бары</Link>
-					<Link href="#">Фаст-Фуд</Link>
+					<Link className='header__link' href="#">Рестораны</Link>
+					<Link className='header__link' href="#">Бары</Link>
+					<Link className='header__link' href="#">Фаст-Фуд</Link>
 				</div>
-				<Link href={`#`}>Контакты</Link>
-				<Link href={`#`}>Войти</Link>
+
+				<button className={`${'button'} ${'header__link'}`} onClick={handleDropdownEntertaiment}>Развлечения<span> &#x25BC;</span></button>
+				<div className={`${dropdownStyleEntertaiment} ${'header__dropdown'}`}>
+					<Link className='header__link' href="#">Кинотеатры</Link>
+					<Link className='header__link' href="#">Парки</Link>
+					<Link className='header__link' href="#">Торговые центры</Link>
+				</div>
+
+				<button className={`${'button'} ${'header__link'}`} onClick={handleDropdownShopping}>Шопинг<span> &#x25BC;</span></button>
+				<div className={`${dropdownStyleShopping} ${'header__dropdown'}`}>
+					<Link className='header__link' href="#">Одежда</Link>
+					<Link className='header__link' href="#">Спорт</Link>
+					<Link className='header__link' href="#">Бижутерия</Link>
+				</div>
+
+				<Link className='header__link' href={`#`}>Контакты</Link>
+				<Link className='header__link' href={`#`}>Войти</Link>
 			</div>
 
 		</div>
